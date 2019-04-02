d695b90ee34483edf9c79ef364826774175ccd65
Yishay1 <seroussi1@gmail.com>
2019-04-02 12:04:16 +0300

    start implement Quadcopter

--- Simulation/src/Quadcopter.java
+++ Simulation/src/Quadcopter.java
@@ -1,4 +1,30 @@
 
 public class Quadcopter {
+	Lidar front;
+	Lidar right;
+	Lidar left;
+	Lidar back;
+	OF of;
+	Yaw yaw;
+	
+	public Quadcopter(Lidar front, Lidar right, Lidar left, Lidar back, OF of, Yaw yaw)
+	{
+		this.front=front;
+		this.right=right;
+		this.left=left;
+		this.back=back;
+		this.of=of;
+		ths.yaw=yaw;
+	}
+	
+	public void start()
+	{
+		
+	}
+	
+	public computeWay()
+	{
+		
+	}
 
 }
