a827327eadd1a8ab43085997e12b486fd5e1583c
Yishay1 <seroussi1@gmail.com>
2019-04-29 10:36:29 +0300

    some order in the code and start add back home by junction(not work well yet

--- Simulation/src/Algorithms/Algo3.java
+++ Simulation/src/Algorithms/Algo3.java
@@ -26,43 +26,8 @@ public class Algo3 extends NavigationAlgorithm{
 			double right_dist=quad.getRightLidar().getCurrentDist();
 			double left_dist=quad.getLeftLidar().getCurrentDist();
 			double front_dist=quad.getFrontLidar().getCurrentDist();
-			boolean is_right_jct=false, is_left_jct= false;
-			if(right_dist-last_right_dist>=1.0) {
-				is_right_jct =true;
-				System.out.println("RIGHT JCT!");
-			}
-			if(left_dist-last_left_dist>=1.0) {
-				is_left_jct =true;
-				System.out.println("LEFT JCT!");
-			}
+			computeAndAddJunction(right_dist, left_dist, front_dist);
 			
-
-			if(is_left_jct||is_right_jct) {
-				boolean addJunction = true;
-				for (Junction jct : graph) 
-				{
-
-					if(quad.getPosition().isWithinRadius(1.5, jct.getCoordinate(), this.quad.getResolution())) 
-					{
-						addJunction = false;
-					}
-				}
-				if (addJunction) {
-					if(is_right_jct) {
-						double di= (right_dist*Math.cos(quad.getRightAngle())+last_right_dist*Math.cos(quad.getRightAngle()))/2;
-						Coordinate junction = new Coordinate((int) (quad.getPosition().x + di*quad.getResolution()*Math.sin(Math.toRadians(quad.getAngle()))),(int) (quad.getPosition().y + di*quad.getResolution()*Math.cos(Math.toRadians(quad.getAngle()))));
-						graph.add(new Junction(junction,is_right_jct,is_left_jct));
-						gui.addJunction(junction.x, junction.y, 4);
-					}
-					else {
-						double di= (left_dist*Math.cos(quad.getRightAngle())+last_left_dist*Math.cos(quad.getRightAngle()))/2;
-						Coordinate junction = new Coordinate((int) (quad.getPosition().x + di*quad.getResolution()*Math.sin(Math.toRadians(quad.getAngle()))),(int) (quad.getPosition().y + di*quad.getResolution()*Math.cos(Math.toRadians(quad.getAngle()))));
-						graph.add(new Junction(junction,is_right_jct,is_left_jct));
-						gui.addJunction(junction.x, junction.y, 4);
-					}
-				}
-			}
-
 			last_right_dist = right_dist;
 			last_left_dist = left_dist;
 			if(quad.getVelocity()<=front_dist/1.2 && front_dist>=0.35) {
@@ -91,6 +56,19 @@ public class Algo3 extends NavigationAlgorithm{
 			System.out.println("jcts: " + graph.size());
 			gui.drive();
 		}
+	}
+
+	private void goTonextJunction() {
+		System.out.println("trying get junction: " + (graph.size()-1));
+		Junction junc=graph.get(graph.size()-1);
+		double ang=quad.getPosition().getAngle(junc.getCoordinate());
+		if(quad.getAngle()-ang>30)
+			quad.turnLeft(1.0);
+		else if(ang-quad.getAngle()>30)
+			quad.turnRight(1.0);
+		if(junc.isInJunction(quad.getPosition()) && graph.size()-1>0)
+			this.graph.remove(graph.size()-1);	
+		
 	}
 
 	private void avoidAlert(double right_dist, double left_dist,double front_dist) {
@@ -135,5 +113,43 @@ public class Algo3 extends NavigationAlgorithm{
 			gui.drive();
 		}
 
+	}
+	private void computeAndAddJunction(double right_dist, double left_dist,double front_dist) {
+		boolean is_right_jct=false, is_left_jct= false;
+		if(right_dist-last_right_dist>=1.0) {
+			is_right_jct =true;
+			System.out.println("RIGHT JCT!");
+		}
+		if(left_dist-last_left_dist>=1.0) {
+			is_left_jct =true;
+			System.out.println("LEFT JCT!");
+		}
+		
+
+		if(is_left_jct||is_right_jct) {
+			boolean addJunction = true;
+			for (Junction jct : graph) 
+			{
+
+				if(quad.getPosition().isWithinRadius(1.5, jct.getCoordinate(), this.quad.getResolution())) 
+				{
+					addJunction = false;
+				}
+			}
+			if (addJunction) {
+				if(is_right_jct) {
+					double di= (right_dist*Math.cos(quad.getRightAngle())+last_right_dist*Math.cos(quad.getRightAngle()))/2;
+					Coordinate junction = new Coordinate((int) (quad.getPosition().x + di*quad.getResolution()*Math.sin(Math.toRadians(quad.getAngle()))),(int) (quad.getPosition().y + di*quad.getResolution()*Math.cos(Math.toRadians(quad.getAngle()))));
+					graph.add(new Junction(junction,is_right_jct,is_left_jct));
+					gui.addJunction(junction.x, junction.y, 4);
+				}
+				else {
+					double di= (left_dist*Math.cos(quad.getRightAngle())+last_left_dist*Math.cos(quad.getRightAngle()))/2;
+					Coordinate junction = new Coordinate((int) (quad.getPosition().x + di*quad.getResolution()*Math.sin(Math.toRadians(quad.getAngle()))),(int) (quad.getPosition().y + di*quad.getResolution()*Math.cos(Math.toRadians(quad.getAngle()))));
+					graph.add(new Junction(junction,is_right_jct,is_left_jct));
+					gui.addJunction(junction.x, junction.y, 4);
+				}
+			}
+		}
 	}
 }
